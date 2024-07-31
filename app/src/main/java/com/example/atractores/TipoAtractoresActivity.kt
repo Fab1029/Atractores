a93107684ee688b185da0dbab612c1104a844357
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2024-07-30 20:39:50 -0500

    Se finalizo interfaces graficas

---
+++ app/src/main/java/com/example/atractores/TipoAtractoresActivity.kt
@@ -0,0 +1,20 @@
+package com.example.atractores
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TipoAtractoresActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tipo_atractores)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
